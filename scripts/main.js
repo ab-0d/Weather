68cc86c01533d945938f86a57e8d220f1dc8fd0e
ab-0d <abdullhm500@gmail.com>
2024-07-24 14:20:13 +0300

    Update the images and improve js code

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
+// Select elements
 let temp = document.querySelector(".temperature .temp .deg");
 let minTemp = document.querySelector(".temperature .min-temp .deg");
 let maxTemp = document.querySelector(".temperature .max-temp .deg");
@@ -8,9 +9,10 @@ let time = document.querySelector(".time-location .time span");
 let day = document.querySelector(".time-location .day");
 let searchInput = document.querySelector(".search-bar input");
 let searchIcon = document.querySelector(".searchbar-icon");
-const apiKey = "8cc349adda44ee5217819693cb6482dc";
+let tepmIcon = document.querySelector(".temp i");
+const apiKey = OPENWEATHERMAP_API_KEY;
 let cityName = "";
-
+// Function to fetch weather data from OpenWeatherMap API
 const fetchData = async (cityName) => {
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
   try {
@@ -23,13 +25,39 @@ const fetchData = async (cityName) => {
     temp.innerHTML = Math.round(data.main.temp - 273.15) + "°C";
     minTemp.innerHTML = Math.floor(data.main.temp_min - 273.15) + "°C";
     maxTemp.innerHTML = Math.ceil(data.main.temp_max - 273.15) + "°C";
-    wind.innerHTML = data.wind.speed + "M/S";
+    wind.innerHTML = data.wind.speed + "km/h";
     humidity.innerHTML = data.main.humidity;
+    changeIcon(data);
   } catch (error) {
     console.log(error);
   }
 };
-
+// Function to change the weather icon
+function changeIcon(data) {
+  let arr = tepmIcon.className.split(" ");
+  // Remove existing icon classes
+  tepmIcon.classList.remove(arr[0]);
+  tepmIcon.classList.remove(arr[1]);
+  // Add new icon classes based on weather condition
+  if (data.weather[0].main == "Clouds") {
+    tepmIcon.classList.add("fa-solid");
+    tepmIcon.classList.add("fa-cloud");
+    tepmIcon.style.color = "white";
+  } else if (data.weather[0].main == "Clear") {
+    tepmIcon.classList.add("fas");
+    tepmIcon.classList.add("fa-sun");
+    tepmIcon.style.color = "#ffb200";
+  } else if (data.weather[0].main == "Rain") {
+    tepmIcon.classList.add("fas");
+    tepmIcon.classList.add("fa-cloud-showers-heavy");
+    tepmIcon.style.color = "white";
+  } else if (data.weather[0].main == "Drizzle") {
+    tepmIcon.classList.add("fas");
+    tepmIcon.classList.add("fa-cloud-rain");
+    tepmIcon.style.color = "white";
+  }
+}
+// Set current day and time in the DOM
 let data = new Date();
 const daysOfWeek = [
   "Sunday",
@@ -47,6 +75,7 @@ time.innerHTML = data.toLocaleTimeString([], {
   hour: "2-digit",
   minute: "2-digit",
 });
+// Event listeners for search functionality
 searchInput.addEventListener("keydown", (event) => {
   if (event.key == "Enter") {
     cityName = searchInput.value;
